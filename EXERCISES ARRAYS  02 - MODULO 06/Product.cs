363f54a0746ba0049131d3d2d92d2136b6b0b06d
Marcus Bianchi <82729031+CmdBianchi@users.noreply.github.com>
2021-04-28 20:24:43 -0300

    Adicionar arquivos de projeto.

---
+++ EXERCISES ARRAYS  02 - MODULO 06/Product.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace EXERCISES_ARRAYS__02___MODULO_06 {
+    //------------------------------- START -------------------------------//
+    class Product {
+        public string Name { get; set; }
+        public double Price { get; set; }
+    }
+    //-------------------------------- END -------------------------------//
+}
---
+++ EXERCISES ARRAYS  02 - MODULO 06/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace EXERCISES_ARRAYS__02___MODULO_06{
+    //-----> INICIO
+    ////////////////////////////////////////////////////////////////////////////////////////////
+    class Program {
+        static void Main(string[] args) { // ----- > VETOR COM DOIS TIPOS DE ARGUMENTOS | DEVE TER CLASSE PARA QUE FUNCIONE 
+            int n = int.Parse(Console.ReadLine());
+
+            Product[] vect = new Product[n];
+
+            for (int i = 0; i < n; i++) {
+                string name = Console.ReadLine();
+                double price = double.Parse(Console.ReadLine());
+                vect[i] = new Product { Name = name, Price = price };
+            }
+            double sum = 0.0;
+            for (int i = 0; i < n; i++)  {
+                sum += vect[i].Price;
+            }
+            double avg = sum / n;
+            Console.WriteLine("AVERAGE PRICE = " + avg.ToString("F2"));
+        }
+    }
+    ////////////////////////////////////////////////////////////////////////////////////////////
+    //-----> FIM
+}
